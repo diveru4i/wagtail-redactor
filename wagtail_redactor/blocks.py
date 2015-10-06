ed3f632a7bdf1d2d70db0ecdec60911531abd632
Rabbit Silver <diveru4i@gmail.com>
2015-10-06 12:45:41 +0300

    разнес статику на 2 хука, подправил RedactorFieldBlock

--- wagtail_redactor/blocks.py
+++ wagtail_redactor/blocks.py
@@ -5,10 +5,13 @@ from .fields import RedactorField
 
 
 class RedactorFieldBlock(blocks.FieldBlock):
-    def __init__(self, **kwargs):
-        self.field = RedactorField()
-        super(RedactorFieldBlock, self).__init__(**kwargs)
+    def __init__(self, required=True, help_text=None, **kwargs):
+        self.field = RedactorField(required=required, help_text=help_text)
+        super(RedactorFieldBlock, self).__init__(required=required, help_text=help_text, **kwargs)
 
     class Meta:
         icon = 'pilcrow icon-redactor'
         template = 'redactor.html'
+
+    def get_searchable_content(self, value):
+        return [force_text(value)]
--- wagtail_redactor/wagtail_hooks.py
+++ wagtail_redactor/wagtail_hooks.py
@@ -6,7 +6,7 @@ from wagtail.wagtailcore import hooks
 
 
 @hooks.register('insert_editor_js')
-def redactor_media():
+def redactor_js():
     js = [
         '/static/redactor/codemirror/lib/codemirror.js',
         '/static/redactor/codemirror/mode/xml/xml.js',
@@ -17,6 +17,14 @@ def redactor_media():
         '/static/redactor/langs/ru.js',
         '/static/redactor/redactor_init_hook.js',
     ]
+
+    js_includes = '\n'.join(['\n<script type="text/javascript" src="%s"></script>' % filename for filename in js])
+
+    return js_includes
+
+
+@hooks.register('insert_editor_css')
+def redactor_css():
     css = [
         '/static/redactor/codemirror/lib/codemirror.css',
         '/static/redactor/css/redactor.css',
@@ -24,7 +32,6 @@ def redactor_media():
         '/static/redactor/css/admin_styles.css',
     ]
 
-    js_includes = '\n'.join(['<script type="text/javascript" src="%s"></script>' % filename for filename in js])
-    css_includes = '\n'.join(['<link type="text/css" rel="stylesheet" href="%s">' % filename for filename in css])
+    css_includes = '\n'.join(['\n<link type="text/css" rel="stylesheet" href="%s">' % filename for filename in css])
 
-    return js_includes + css_includes
+    return css_includes
