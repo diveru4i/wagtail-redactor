3c3dbe3c72164e7572809b1af2f3a8cd70d58588
Rabbit Silver <diveru4i@gmail.com>
2015-12-08 03:49:01 +0300

    added PatchedRedactorField for vanilla use

--- wagtail_redactor/fields.py
+++ wagtail_redactor/fields.py
@@ -1,6 +1,10 @@
+from redactor.fields import RedactorField as DefaultRedactorField
+from redactor.widgets import RedactorEditor
+
 from django.forms.fields import CharField
 from django.conf import settings
-from redactor.widgets import RedactorEditor
+
+from .widgets import PatchedRedactorEditor
 
 
 class RedactorField(CharField):
@@ -16,6 +20,21 @@ class RedactorField(CharField):
             allow_image_upload=allow_image_upload
         )
         super(RedactorField, self).__init__(*args, **kwargs)
+
+
+class PatchedRedactorField(DefaultRedactorField):
+    def __init__(self, *args, **kwargs):
+        super(PatchedRedactorField, self).__init__(*args, **kwargs)
+        options = kwargs.pop('redactor_options', {})
+        upload_to = kwargs.pop('upload_to', '')
+        allow_file_upload = kwargs.pop('allow_file_upload', True)
+        allow_image_upload = kwargs.pop('allow_image_upload', True)
+        self.widget = PatchedRedactorEditor(
+            redactor_options=options,
+            upload_to=upload_to,
+            allow_file_upload=allow_file_upload,
+            allow_image_upload=allow_image_upload
+        )
 
 
 if 'south' in settings.INSTALLED_APPS:
